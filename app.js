24c50fc5adc0048fa471850ffa53ddf0f2a96090
Jonas Franz <jonas.franz@mailbox.org>
2022-08-29 14:00:40 +0200

    Calculator working

--- app.js
+++ app.js
@@ -1,34 +1,99 @@
 let clickedNumber = "";
 
-const current = document.querySelector(".current");
-let currentText = current.textContent;
+let firstNumber = "";
+let operation = "";
+let secondNumber = "";
 
-const buttons = document.querySelectorAll(".number-button");
-buttons.forEach((button) => {
+let operationSign = "";
+let result = "";
+let isResultInCurrent = false;
+
+const current = document.querySelector(".current");
+const currentP = document.querySelector(".current p");
+const history = document.querySelector(".history");
+
+const numberButtons = document.querySelectorAll(".number-button");
+
+numberButtons.forEach((button) => {
   button.addEventListener("click", function () {
     clickedNumber = this.textContent;
 
-    const p = document.querySelector(".current p");
-    p.textContent += this.textContent;
+    if (isResultInCurrent) {
+      currentP.textContent = "";
+      isResultInCurrent = false;
+    }
+
+    currentP.textContent += this.textContent;
+    operation
+      ? (secondNumber += this.textContent)
+      : (firstNumber += this.textContent);
+    console.log("firstNumber: " + firstNumber);
+    console.log("secondNumber: " + secondNumber);
   });
 });
 
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    operation = this.id;
+
+    operationSign = this.textContent;
+    currentP.textContent += operationSign;
+
+    console.log(operation);
+  });
+});
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", function () {
+  console.log("operation: " + operation);
+  result = operate(operation, firstNumber, secondNumber);
+
+  const newHistoryElement = document.createElement("p");
+  newHistoryElement.textContent =
+    firstNumber + operationSign + secondNumber + "=" + result;
+  history.appendChild(newHistoryElement);
+
+  currentP.textContent = result;
+  isResultInCurrent = true;
+
+  firstNumber = "";
+  secondNumber = "";
+  operation = "";
+  operationSign = "";
+  result = "";
+});
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", function () {
+  clickedNumber = "";
+  firstNumber = "";
+  operation = "";
+  secondNumber = "";
+  result = "";
+
+  currentP.textContent = "";
+  while (history.hasChildNodes()) {
+    history.removeChild(history.lastChild);
+  }
+});
+
 function operate(operator, num1, num2) {
-  return operator(num1, num2);
+  return window[operator](num1, num2);
 }
 
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 function substract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
 }
