5497ad214c6098cf5163dd7c2359d6ecc72c3ec0
Jonas Franz <jonas.franz@mailbox.org>
2022-08-29 12:51:53 +0200

    Numbers visible in Current

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+let clickedNumber = "";
+
+const current = document.querySelector(".current");
+let currentText = current.textContent;
+
+const buttons = document.querySelectorAll(".number-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    clickedNumber = this.textContent;
+
+    const p = document.querySelector(".current p");
+    p.textContent += this.textContent;
+  });
+});
+
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
